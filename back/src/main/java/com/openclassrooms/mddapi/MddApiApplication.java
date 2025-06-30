e4019f0da82fcccefee81f00e09866fef2c44f19
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-06-30 15:07:59 +0200

    🐛 Fix install MVN

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -2,8 +2,9 @@ package com.openclassrooms.mddapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MddApiApplication {
 
 	public static void main(String[] args) {
