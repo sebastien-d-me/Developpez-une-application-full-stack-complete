cb2a33396dd981c57b25871bf7d9792b4f92b904
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-06-30 15:45:50 +0200

    🎨 Register + Login (juste composants)

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, RegisterComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
