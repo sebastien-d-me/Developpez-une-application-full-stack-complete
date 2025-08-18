0ed4aecdfe141c5b35ea662fe9f9da8aaa69fd6d
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-08-18 20:44:44 +0200

    🧱 Date commentaire + bon ordre

--- front/src/app/components/posts/comment/comment.component.ts
+++ front/src/app/components/posts/comment/comment.component.ts
@@ -1,10 +1,11 @@
+import { DatePipe } from "@angular/common";
 import { Component, Input } from "@angular/core";
 
 
 @Component({
     selector: "app-comment",
     standalone: true,
-    imports: [],
+    imports: [DatePipe],
     templateUrl: "./comment.component.html",
     styleUrl: "./comment.component.scss"
 })
@@ -14,4 +15,5 @@ export class CommentComponent {
     /* Input */
     @Input() author : string = "";
     @Input() text : string = "";
+    @Input() date: string = "";
 }
